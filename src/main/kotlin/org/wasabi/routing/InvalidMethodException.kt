32f04a3f302cac39582caa0facad6677f78b7ba0
Maxim Mazin <Maxim.Mazin@jetbrains.com>
2014-06-23 21:51:53 +0200

    Migrate to Kotlin 0.7.1217

--- src/main/kotlin/org/wasabi/routing/InvalidMethodException.kt
+++ src/main/kotlin/org/wasabi/routing/InvalidMethodException.kt
@@ -2,5 +2,5 @@ package org.wasabi.routing
 
 import io.netty.handler.codec.http.HttpMethod
 
-public class InvalidMethodException(val message: String = "Invalid method exception", val allowedMethods: Array<HttpMethod>) : Exception(message) {
+public class InvalidMethodException(message: String = "Invalid method exception", val allowedMethods: Array<HttpMethod>) : Exception(message) {
 }
