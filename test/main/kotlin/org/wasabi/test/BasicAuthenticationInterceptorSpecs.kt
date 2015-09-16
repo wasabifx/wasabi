80f39e3b4aa92fc1feb5029c5eafd367f9718630
Dale Anderson <swishyTM@gmail.com>
2015-09-16 22:05:08 +1200

    Fix up interceptor assignment in basic auth tests, add test accessing non auth route on server with auth'd route.

--- test/main/kotlin/org/wasabi/test/BasicAuthenticationInterceptorSpecs.kt
+++ test/main/kotlin/org/wasabi/test/BasicAuthenticationInterceptorSpecs.kt
@@ -3,35 +3,35 @@ package org.wasabi.test
 import org.wasabi.authentication.BasicAuthentication
 import org.wasabi.interceptors.AuthenticationInterceptor
 import org.wasabi.interceptors.useAuthentication
-import org.junit.Test as spec
 import kotlin.test.assertEquals
+import org.junit.Test as spec
 
 public class BasicAuthenticationInterceptorSpecs : TestServerContext() {
 
     @spec fun requesting_a_protected_resource_should_return_authentication_required () {
 
-        TestServer.appServer.useAuthentication(BasicAuthentication("protected", { user, pass -> user == pass }, "/protected"))
+        TestServer.appServer.useAuthentication(BasicAuthentication("protected", { user, pass -> user == pass }), "/protected")
 
-        TestServer.appServer.get("/protected", { response.send("This should be proctected")})
+        TestServer.appServer.get("/protected", { response.send("This should be protected")})
 
         val response = get("http://localhost:${TestServer.definedPort}/protected", hashMapOf())
 
         assertEquals(401, response.statusCode)
 
-        TestServer.appServer.interceptors.remove(3)
+        TestServer.appServer.interceptors.remove(BasicAuthentication("protected", { user, pass -> user == pass }))
     }
 
     @spec fun requesting_a_unprotected_resource_should_return_success () {
 
-        TestServer.appServer.useAuthentication(BasicAuthentication("protected", { user, pass -> user == pass }, "/protected"))
+        TestServer.appServer.useAuthentication(BasicAuthentication("protected", { user, pass -> user == pass }),"/protected")
 
         TestServer.appServer.get("/protected", { response.send("This should be proctected")})
 
-        val response = get("http://localhost:${TestServer.definedPort}/", hashMapOf())
+        TestServer.appServer.get("/notprotected", { response.send("This should not be protected")})
+
+        val response = get("http://localhost:${TestServer.definedPort}/notprotected", hashMapOf())
 
         assertEquals(200, response.statusCode)
-
-        TestServer.appServer.interceptors.remove(4)
     }
 
 }
