74a54fa5c5dba4f66ae4cd0b75ea89169f766e8e
Antanas Končius <antanas@bassline.lt>
2016-03-12 19:44:12 +0200

    Fixed issue with AutoOptionsInterceptor when routes have parameters + added several response headers for CORS support

--- src/main/kotlin/org/wasabi/interceptors/AutoOptionsInterceptor.kt
+++ src/main/kotlin/org/wasabi/interceptors/AutoOptionsInterceptor.kt
@@ -7,17 +7,23 @@ import io.netty.handler.codec.http.HttpMethod
 import java.util.ArrayList
 import org.wasabi.routing.Route
 import org.wasabi.http.StatusCodes
+import org.wasabi.routing.PatternAndVerbMatchingRouteLocator
 
 public class AutoOptionsInterceptor(val routes: ArrayList<Route>): Interceptor() {
     override fun intercept(request: Request, response: Response): Boolean {
         var executeNext = false
         if (request.method == HttpMethod.OPTIONS) {
-            val methods = routes.filter {
-                it.path == request.path
-            }.map {
-                it.method
-            }
-            response.addRawHeader("Allow", methods.joinToString(", "))
+            val routeLocator = PatternAndVerbMatchingRouteLocator(routes)
+            val allowedMethods = routes
+                .filter { routeLocator.compareRouteSegments(it, request.path) }
+                .map { it.method }
+                .toTypedArray()
+
+
+            response.setAllowedMethods(allowedMethods)
+            response.addRawHeader("Access-Control-Allow-Origin", "*")
+            response.addRawHeader("Access-Control-Allow-Methods", allowedMethods.joinToString(", "))
+            response.addRawHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
             response.setStatus(StatusCodes.OK)
         } else {
             executeNext = true
