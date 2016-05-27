4d88ebe0d1b5432bb1f9e496464969f254030ef2
Dale Anderson <condaa1@apteryx.local>
2016-05-27 20:16:13 +1200

    Add further test to body params suite, actually verifys body param is read.

--- test/main/kotlin/org/wasabi/test/BodyParametersSpecs.kt
+++ test/main/kotlin/org/wasabi/test/BodyParametersSpecs.kt
@@ -3,6 +3,7 @@ package org.wasabi.test
 import org.junit.Test as spec
 import org.apache.http.NameValuePair
 import org.apache.http.message.BasicNameValuePair
+import org.wasabi.routing.routeHandler
 import java.util.ArrayList
 import kotlin.test.assertEquals
 
@@ -10,6 +11,11 @@ import kotlin.test.assertEquals
  * Created by swishy on 25/11/15.
  */
 public class BodyParametersSpecs : TestServerContext() {
+
+    val createMember = routeHandler {
+        var memberName = request.bodyParams["memberName"] as String?
+        assert(memberName == "bob")
+    }
 
     @spec fun requests_should_have_isolated_body_params () {
 
@@ -31,6 +37,15 @@ public class BodyParametersSpecs : TestServerContext() {
 
         assert(response2.body.contains("1"))
 
+    }
 
+    @spec fun body_params_present_on_request () {
+
+        TestServer.appServer.post("/body", createMember)
+
+        var urlParameters3  = ArrayList<NameValuePair>();
+        urlParameters3.add(BasicNameValuePair("memberName", "bob"));
+
+        post("http://localhost:${TestServer.definedPort}/body", hashMapOf(), urlParameters3)
     }
 }
