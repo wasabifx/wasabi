b42da12e902ce8512e2c23912f3894de4d3383e5
Dale Anderson <swishyTM@gmail.com>
2016-07-07 22:50:33 +1200

    CORS interceptor comments

--- src/main/kotlin/org/wasabi/interceptors/CORSInterceptor.kt
+++ src/main/kotlin/org/wasabi/interceptors/CORSInterceptor.kt
@@ -14,9 +14,13 @@ public class CORSInterceptor(val routes: ArrayList<Route>, val settings: ArrayLi
     override fun intercept(request: Request, response: Response): Boolean {
         for (setting in settings) {
             if (setting.path == "*" || request.path.matches(setting.path.toRegex())) {
+
+                // This covers non options requests, browser expects the below on the
+                // request subsequent to the options request on CORS transfers.
                 if (response.statusCode == StatusCodes.OK.code) {
                     response.addRawHeader("Access-Control-Allow-Origin", setting.path)
                 }
+                // This handles the initial OPTIONS request during the CORS transfer.
                 if (request.method == HttpMethod.OPTIONS) {
                     val methods = routes.filter {
                         it.path == request.path
