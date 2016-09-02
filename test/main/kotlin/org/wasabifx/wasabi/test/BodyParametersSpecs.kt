5f048a971b11891a5c5e856643b5c3496024d7e4
hadihariri <mail@hadihariri.com>
2016-09-02 10:02:16 +0200

    Some minor cleanup on var/val.

--- test/main/kotlin/org/wasabifx/wasabi/test/BodyParametersSpecs.kt
+++ test/main/kotlin/org/wasabifx/wasabi/test/BodyParametersSpecs.kt
@@ -16,33 +16,33 @@ class BodyParametersSpecs : TestServerContext() {
         assert(memberName == "bob")
     }
 
-    @spec fun requests_should_have_isolated_body_params () {
+    @spec fun requests_should_have_isolated_body_params() {
 
         TestServer.appServer.post("/params", {
             response.send(request.bodyParams.size.toString())
         })
 
-        var urlParameters  = ArrayList<NameValuePair>()
+        val urlParameters = ArrayList<NameValuePair>()
         urlParameters.add(BasicNameValuePair("foo", "bar"))
 
-        var urlParameters2  = ArrayList<NameValuePair>()
+        val urlParameters2 = ArrayList<NameValuePair>()
         urlParameters2.add(BasicNameValuePair("bar", "baz"))
 
-        var response = post("http://localhost:${TestServer.definedPort}/params", hashMapOf(), urlParameters)
+        val response = post("http://localhost:${TestServer.definedPort}/params", hashMapOf(), urlParameters)
 
         assert(response.body.contains("1"))
 
-        var response2 = post("http://localhost:${TestServer.definedPort}/params", hashMapOf(), urlParameters2)
+        val response2 = post("http://localhost:${TestServer.definedPort}/params", hashMapOf(), urlParameters2)
 
         assert(response2.body.contains("1"))
 
     }
 
-    @spec fun body_params_present_on_request () {
+    @spec fun body_params_present_on_request() {
 
         TestServer.appServer.post("/body", createMember)
 
-        var urlParameters3  = ArrayList<NameValuePair>()
+        var urlParameters3 = ArrayList<NameValuePair>()
         urlParameters3.add(BasicNameValuePair("memberName", "bob"))
 
         post("http://localhost:${TestServer.definedPort}/body", hashMapOf(), urlParameters3)
