528ac344e29e25d641b9c2462635d195c3ffe05e
Tradunsky <v.tradunskih@gmail.com>
2016-10-28 02:33:36 +0300

    made search similar path when registration a new route to avoid paths conflicts

--- src/main/kotlin/org/wasabifx/wasabi/app/AppServer.kt
+++ src/main/kotlin/org/wasabifx/wasabi/app/AppServer.kt
@@ -1,6 +1,5 @@
 package org.wasabifx.wasabi.app
 
-import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.http.HttpMethod
 import org.slf4j.LoggerFactory
 import org.wasabifx.wasabi.deserializers.Deserializer
@@ -27,7 +26,7 @@ open class AppServer(val configuration: AppConfiguration = AppConfiguration()) {
     private val httpServer: HttpServer
     private var running = false
 
-    val routes: ArrayList<Route> = ArrayList<Route>()
+    val routes: MutableSet<Route> = mutableSetOf()
     val channels: ArrayList<Channel> = ArrayList<Channel>()
     val exceptionHandlers: MutableSet<RouteException> = mutableSetOf()
     val interceptors : ArrayList<InterceptorEntry>  = ArrayList<InterceptorEntry>()
@@ -40,9 +39,9 @@ open class AppServer(val configuration: AppConfiguration = AppConfiguration()) {
     }
 
     private fun addRoute(method: HttpMethod, path: String, vararg handler: RouteHandler.() -> Unit) {
-        val existingRoute = routes.filter { it.path == path && it.method == method }
-        if (existingRoute.count() >= 1) {
-            throw RouteAlreadyExistsException(existingRoute.firstOrNull()!!)
+        val existingRoute = routes.findSimilar(method, path)
+        if (existingRoute != null) {
+            throw RouteAlreadyExistsException(existingRoute)
         }
         routes.add(Route(path, method, HashMap<String, String>(), *handler))
     }
