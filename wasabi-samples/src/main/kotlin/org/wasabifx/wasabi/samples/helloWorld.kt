6fc595d8d19d1192e8ade3ce7d96ef7c19a2a168
hadihariri <mail@hadihariri.com>
2016-11-02 08:17:27 +0100

    Merged changes for File Upload

--- wasabi-samples/src/main/kotlin/org/wasabifx/wasabi/samples/helloWorld.kt
+++ wasabi-samples/src/main/kotlin/org/wasabifx/wasabi/samples/helloWorld.kt
@@ -1,10 +1,11 @@
 package org.wasabifx.wasabi.samples
 
+import org.wasabifx.wasabi.app.AppConfiguration
 import org.wasabifx.wasabi.app.AppServer
 
 fun main(args: Array<String>) {
 
-    val server = AppServer()
+    val server = AppServer(AppConfiguration(enableLogging = false))
 
     server.get("/", {
         response.send("Hello World!")
